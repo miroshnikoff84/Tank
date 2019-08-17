e76965a06747b3671ce88cb715a22ce91c362839
miroshnikoff84 <miroshnikoff84@mail.ru>
2019-08-18 00:21:10 +0300

    Upgrade

--- TankRCavto.ino
+++ TankRCavto.ino
@@ -1,14 +1,14 @@
 int pot1 = A1; // pin pot1  ГАЗ
 int pot2 = A2; // pin pot2 РУЛЬ
-int IN1 = 8; //input1 подключен к выводу 8 
+int IN1 = 8;  
 int IN2 = 7;
 int IN3 = 2;
 int IN4 = 4;
 int EN1 = 9;
 int EN2 = 10;
 
-int motr;  // speed motor right
-int motl;  // speed motor left
+int motr;  // скорость правого мотора
+int motl;  // скорость левого мотора
 int mr;
 int ml;
 int will;
@@ -25,7 +25,6 @@ void loop() {
   will = analogRead(pot2);
   will = map(will,0, 1023, -250, 250);
   
-  //Serial.print("\t");
   if(-5>will<5){
     motr=mr;
     motl=ml;
